212aa3eb5e4205a43a0960378db48252c8b3b163
kawa-yo <yoshito.kawasaki.pub@gmail.com>
2023-02-06 22:46:15 +0900

    delete unsable files

--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-from .derm_tiff_image import DermTiffImage, load_image
-
-__all__ = ["DermTiffImage", "load_image"]
--- src/io.py
+++
@@ -1,8 +0,0 @@
-
-import os
-
-
-def make_parent_dir(file: str) -> None:
-    dirname = os.path.dirname(file)
-    if dirname.strip() != '':
-        os.makedirs(dirname, exist_ok=True)
