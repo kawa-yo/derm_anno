b87d9fa8851d960af1eee5de14b723ac15c9e870
kawa-yo <yoshito.kawasaki.pub@gmail.com>
2023-02-06 22:43:42 +0900

    add examples

---
+++ example/example.py
@@ -0,0 +1,44 @@
+from derm_tiff import load_image
+from PIL import Image
+import os
+import numpy as np
+
+
+def main():
+    # DermAnnotationで作成されたTIFF画像を開く
+    derm_image = load_image("example/imgs/input.tiff")
+
+    # TIFF画像の各フレームの名前を取得
+    labels = derm_image.labels
+    print(labels)  # ['Pencil1', 'Pencil2', 'Pencil3']
+    print(derm_image.label2color)
+
+    # TIFF画像の各フレームのマスクを合成して出力
+    for label in labels:
+        img = derm_image.get_annotation_image([label], alpha=0.5)
+        Image.fromarray(img).save(f"example/imgs/with_{label}.png")
+
+    # どのフレームのマスクとも重複しないようなマスクを新たに追加する
+    H, W, _ = derm_image.shape
+    color = (255, 255, 0)
+    new_mask = np.ones((H, W), dtype=np.bool_)
+    for label in labels:
+        mask = derm_image.label2mask[label]
+        new_mask = np.logical_and(new_mask, np.logical_not(mask))
+
+    # ラベルがすでにある場合は追加されない
+    derm_image.remove_frame("empty_space")
+    derm_image.add_frame("empty_space", new_mask, color)
+
+    img = derm_image.get_annotation_image(["empty_space"], alpha=0.5)
+    Image.fromarray(img).save("example/imgs/with_empty_space.png")
+
+    # DermAnnotationで開けるTIFF形式で保存
+    derm_image.save("example/imgs/output.tiff")
+
+    # 全体でリサイズも可能
+    resized = derm_image.resize(300, 200)
+
+
+if __name__ == "__main__":
+    main()
